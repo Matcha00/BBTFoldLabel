b58d1f26c4a9ee7781edb7a5ef31c84502201699
chenhuan <chenhuan@bonbonwork.com>
2019-10-31 17:10:10 +0800

    建立属性字符串分类

---
+++ BBTFoldLabel/Classes/NSAttributedString+BBTFoldLabel.m
@@ -0,0 +1,15 @@
+//
+//  NSAttributedString+BBTFoldLabel.m
+//  BBTFoldLabel
+//
+//  Created by Matcha00 on 2019/10/31.
+//
+
+#import "NSAttributedString+BBTFoldLabel.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation NSAttributedString (BBTFoldLabel)
+
+@end
