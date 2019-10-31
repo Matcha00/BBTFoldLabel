55b7bd9b3e16d4a041fcf8bf538128b3f96099be
chenhuan <chenhuan@bonbonwork.com>
2019-10-31 17:03:27 +0800

    新建源码

---
+++ BBTFoldLabel.h
@@ -0,0 +1,16 @@
+//
+//  BBTFoldLabel.h
+//  BBTFoldLabel
+//
+//  Created by Matcha00 on 2019/10/31.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BBTFoldLabel : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BBTFoldLabel.m
@@ -0,0 +1,20 @@
+//
+//  BBTFoldLabel.m
+//  BBTFoldLabel
+//
+//  Created by Matcha00 on 2019/10/31.
+//
+
+#import "BBTFoldLabel.h"
+
+@implementation BBTFoldLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
